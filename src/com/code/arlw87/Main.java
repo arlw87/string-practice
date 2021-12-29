4fc5b3ba7cdc8f96aea27dd44c1ecb6388c6084c
arlw87 <arlw87@googlemail.com>
2021-12-29 16:39:27 +0000

    initial commit

---
+++ src/com/code/arlw87/Main.java
@@ -0,0 +1,9 @@
+package com.code.arlw87;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.print("This is a program for practicing String methods");
+    }
+}
